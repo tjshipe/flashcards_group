d6cf2c43e06fb4533870f963ce3828663fc1caf5
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-28 17:51:11 -0500

    user auth

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,27 @@
+enable :sessions
+
+
 get '/' do
   @decks = Deck.all
   erb :index
+end
+
+get '/register' do
+  erb :register
+end
+
+post '/register' do
+  new_user = User.new(email: params[:user][:email])
+  new_user.password = params[:user][:password]
+  new_user.save!
+  redirect '/register'
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  session[:user] = User.find_by_email(params[:user][:email])
+  redirect '/'
 end
--- config/environment.rb
+++ config/environment.rb
@@ -22,6 +22,7 @@ require "sinatra/reloader" if development?
 
 require 'erb'
 
+
 # Some helper constants for path-centric logic
 APP_ROOT = Pathname.new(File.expand_path('../../', __FILE__))
 
